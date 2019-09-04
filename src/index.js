27b95a4a471858b2488752d4b84ffcac53cf4dc9
Abhishek Khare <abhishek.khare@broadcom.com>
2019-09-04 14:59:22 -0700

    adding seprate component folder for better org of project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import MyInfo from './MyInfo';
+import MyInfo from "./components/MyInfo"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
