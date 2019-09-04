dcce2a5747622b68d9803b415d2e5babc3e9ebea
Abhishek Khare <abhishek.khare@broadcom.com>
2019-09-04 14:37:52 -0700

    Functional Component Practice

--- src/index.js
+++ src/index.js
@@ -3,19 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Objectives:
+// 1. Set up the basic React code from scratch
+// 2. Create a functional component called MyInfo that returns the following UI:
+// a. An h1 with your name
+// b. A paragraph with a little blurb about yourself
+// c. An ordered or unordered list of the top 3 vacation spots you'd like to visit
+// 3. Render an instance of that functional component to the browser
+// Extra challenge: learn on your own (Google!) how you can add some style to your page.
+// (We will also cover this in an upcoming lesson).
 
-function MyApp() {
+function MyInfo() {
     return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+            <br/>
+            <h1>Abhishek Khare</h1>
+            <br/>
+            <p>Abhishek like to learn new things</p>
+            <br/>
+            <ol>
+                <li>Cancun</li>
+                <li>New York</li>
+                <li>Key West</li>
+            </ol>
+        </div>
     )
 }
 
 ReactDOM.render(
-    <MyApp />,
+    <MyInfo/>,
     document.getElementById("root")
 )
 
