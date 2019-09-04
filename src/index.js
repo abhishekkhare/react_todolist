5d94446bb2a2f8c395a58b50909a1f39280144ba
Abhishek Khare <abhishek.khare@broadcom.com>
2019-09-04 14:33:34 -0700

    Basic Functional Component

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-</ul>, document.getElementById('root'));
+function MyApp() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+ReactDOM.render(
+    <MyApp />,
+    document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
