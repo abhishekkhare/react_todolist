2c2e2e9858c2d4a680f6efbe02bbcd5463b0796a
Abhishek Khare <abhishek.khare@broadcom.com>
2019-09-05 14:31:57 -0700

    React Todo APP -1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react"
+/*
+Time to have fun styling! But first things first: 
 
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer"
+1. Change the input/p combo below to be a new component called <TodoItem />. <TodoItem /> (for now) will just have the same displayed data below (every todo item is the same) hardcoded inside of it. (We'll learn soon how to make the TodoItem more flexible)
+    
+2. Style up the page however you want! You're welcome to use regular CSS (in the CSS file) or inline styles, or both!
+*/
+
+import React from "react"
+import TodoItem from "./components/TodoItem"
 
 function App() {
-  return (
-      <div>
-          <Header />
-        <MainContent />
-        <Footer />
-      </div>
-  )
+    return (
+        <div>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </div>
+    )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// Set up the React app from scratch
-// Render an App component (defined in a separate file)
-// Inside App, render:
-// 1. A Navbar component
-// 2. A MainContent component
-// 3. A Footer component
-
-
 ReactDOM.render(
     <App/>,
     document.getElementById("root")
