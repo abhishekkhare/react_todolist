828522f1f2503c2472f3952cc395edbc1d621d7c
Abhishek Khare <abhishek.khare@broadcom.com>
2019-09-05 14:58:35 -0700

    React Props 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-/*
-Time to have fun styling! But first things first: 
-
-1. Change the input/p combo below to be a new component called <TodoItem />. <TodoItem /> (for now) will just have the same displayed data below (every todo item is the same) hardcoded inside of it. (We'll learn soon how to make the TodoItem more flexible)
-    
-2. Style up the page however you want! You're welcome to use regular CSS (in the CSS file) or inline styles, or both!
-*/
 
 import React from "react"
-import TodoItem from "./components/TodoItem"
+import ContactCard from "./components/ContactCard"
 
 function App() {
     return (
-        <div>
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+        <div className="contacts">
+            <ContactCard
+                name="Mr. Whiskerson"
+                imgUrl="http://placekitten.com/300/200"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+
+            <ContactCard
+                name="Fluffykins"
+                imgUrl="http://placekitten.com/400/200"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+
+            <ContactCard
+                name="Destroyer"
+                imgUrl="http://placekitten.com/400/300"
+                phone="(212) 555-3456"
+                email="ofworlds@yahoo.com"
+            />
+
+            <ContactCard
+                name="Felix"
+                imgUrl="http://placekitten.com/200/100"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
         </div>
     )
 }
