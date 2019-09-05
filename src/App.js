58e39061783a640139243c24ffdc998c71d879ab
Abhishek Khare <abhishek.khare@broadcom.com>
2019-09-05 15:20:37 -0700

    mapping components in React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-
 import React from "react"
-import ContactCard from "./components/ContactCard"
+
+import Joke from "./components/Joke"
+import jokesData from "./data/jokesData"
 
 function App() {
+    const jokeComponents = jokesData.map(joke => <Joke key = {joke.id} question={joke.question} punchLine={joke.punchLine} />)
     return (
-        <div className="contacts">
-            <ContactCard
-                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-            />
 
-            <ContactCard
-                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
-            />
-
-            <ContactCard
-                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
-            />
-
-            <ContactCard
-                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-            />
-
+        <div>
+            {jokeComponents}
         </div>
     )
 }
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -3,8 +3,8 @@ import React from "react"
 function Joke(props) {
     return (
         <div>
-            <h3>Question: {props.question}</h3>
-            <h3>Answer: {props.punchLine}</h3>
+            <h3 style={{display: !props.question && "none"}}>Question: {props.question}</h3>
+            <h3 style={{color: !props.question && "#888888"}}>Answer: {props.punchLine}</h3>
             <hr/>
         </div>
     )
