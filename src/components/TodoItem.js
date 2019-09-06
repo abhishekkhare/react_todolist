0691c1b6fcbdeab8ff39a80919df37b76b03e98e
Abhishek Khare <abhishek.khare@broadcom.com>
2019-09-06 15:58:48 -0700

    React Todo APP -3 - with state

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,7 @@ import React from "react"
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.todoItem.completed}/>
+            <input type="checkbox" checked={props.todoItem.completed} onChange={() => props.handleChange(props.todoItem.id)} />
             <p>{props.todoItem.text}</p>
         </div>
     )
